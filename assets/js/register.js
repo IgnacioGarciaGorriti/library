946b3b29c004cccd177b7ceccd9de4bd3c7754df
LAPTOP-A3NKD4NA\gorri <ignacio.garcia@dracoder.com>
2023-06-29 10:04:58 +0200

    validations

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,20 +1,28 @@
+import {Db} from './Services/Db.js';
+import {User} from './Entity/User.js';
+
 document.addEventListener("DOMContentLoaded", () => {
     const btn_save = document.getElementById("register-save");
+    const db = new Db('table_users');
     btn_save.addEventListener("click", () => {
         try {
             const form = document.getElementById("register");
             const data = new FormData(form);
-            checkFields(data);
+            checkFields(db, data);
+            const id = db.getLastId();
+            const user = new User(id, ...data.values());
         } catch(e) {
             toastr.error(e.message);
         }
     });
 });
 
-const checkFields = (fields) => {
+const checkFields = (db, fields) => {
     fields.forEach((field, key) => {
         if(field.length === 0) {
             throw new Error("El campo " + key + " no puede estar vacío");
+        } else if(key === "nickname" && db.getBy("nickname", field).length !== 0 ) {
+            throw new Error("El nickname ya existe");
         }
     });
 }
