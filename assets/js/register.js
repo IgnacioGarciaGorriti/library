4433d40f0474758b7bf2950e1891effa2ff4d141
Ignacio <ignacio.garcia@dracoder.com>
2023-07-11 09:29:45 +0200

    validate date

--- assets/js/register.js
+++ assets/js/register.js
@@ -8,10 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
         try {
             const form = document.getElementById("register");
             const data = new FormData(form);
-            checkFields(db, data);
+            checkFields(db, data);  
             const id = db.getLastId();
             const user = new User(id, ...data.values());
             db.add(user);
+            //location.href = 'login.html';
         } catch(e) {
             toastr.error(e.message);
         }
@@ -24,8 +25,28 @@ const checkFields = (db, fields) => {
             throw new Error("El campo " + key + " no puede estar vacío");
         } else if(key === "nickname" && db.getBy("nickname", field).length !== 0 ) {
             throw new Error("El nickname ya existe");
+        } else if(key == "email" && !checkEmail(field)) {
+            throw new Error("No es un email válido");
         } else if(key === "email" && db.getBy("email", field).length !== 0 ) {
             throw new Error("El email ya existe");
+        } else if(key === "password" && field !== fields.get("password2")){
+            throw new Error("Las contraseñas no coinciden");
+        } else if(key === "age" && !checkDate(field)){
+            throw new Error("La fecha no puede ser posterior al día de hoy");
         }
     });
+}
+
+const checkEmail = (email) => {
+    return email.toLowerCase().match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+}
+
+const checkDate = (dateString) => {
+    const now = Date.now();
+    const date = Date.parse(dateString);
+    if(now < date) {
+        return false;
+    }
+
+    return true;
 }
